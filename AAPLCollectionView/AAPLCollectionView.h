5571ef221b11099530cd795d9b4096988217099f
Rayman Rosevear <ray@mushroomcloud.co.za>
2015-07-26 20:53:30 +0200

    Initial commit

---
+++ AAPLCollectionView/AAPLCollectionView.h
@@ -0,0 +1,46 @@
+//
+//  AAPLCollectionView.h
+//  AAPLCollectionView
+//
+//  Created by Rayman Rosevear on 2015/07/26.
+//  Copyright (c) 2015 Mushroom Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AAPLCollectionView.
+FOUNDATION_EXPORT double AAPLCollectionViewVersionNumber;
+
+//! Project version string for AAPLCollectionView.
+FOUNDATION_EXPORT const unsigned char AAPLCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AAPLCollectionView/PublicHeader.h>
+
+#import <AAPLCollectionView/AAPLLabel.h>
+#import <AAPLCollectionView/AAPLTheme.h>
+#import <AAPLCollectionView/AAPLAction.h>
+#import <AAPLCollectionView/AAPLBasicCell.h>
+#import <AAPLCollectionView/AAPLDataSource.h>
+#import <AAPLCollectionView/UIView+Helpers.h>
+#import <AAPLCollectionView/AAPLKeyValueCell.h>
+#import <AAPLCollectionView/AAPLStateMachine.h>
+#import <AAPLCollectionView/AAPLLayoutMetrics.h>
+#import <AAPLCollectionView/AAPLTextValueCell.h>
+#import <AAPLCollectionView/AAPLContentLoading.h>
+#import <AAPLCollectionView/AAPLBasicDataSource.h>
+#import <AAPLCollectionView/AAPLPlaceholderView.h>
+#import <AAPLCollectionView/AAPLSectionHeaderView.h>
+#import <AAPLCollectionView/AAPLDataSourceMetrics.h>
+#import <AAPLCollectionView/AAPLCollectionViewCell.h>
+#import <AAPLCollectionView/AAPLComposedDataSource.h>
+#import <AAPLCollectionView/AAPLDataSource+Headers.h>
+#import <AAPLCollectionView/AAPLDataSource_Private.h>
+#import <AAPLCollectionView/AAPLKeyValueDataSource.h>
+#import <AAPLCollectionView/AAPLPinnableHeaderView.h>
+#import <AAPLCollectionView/AAPLSegmentedDataSource.h>
+#import <AAPLCollectionView/AAPLSegmentedHeaderView.h>
+#import <AAPLCollectionView/AAPLTextValueDataSource.h>
+#import <AAPLCollectionView/AAPLCollectionViewLayout.h>
+#import <AAPLCollectionView/AAPLCollectionViewController.h>
+#import <AAPLCollectionView/AAPLDataSourceMetrics_Private.h>
+#import <AAPLCollectionView/AAPLCollectionViewLayoutAttributes.h>
---
+++ AdvancedCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AdvancedCollectionView
+//
+//  Created by Rayman Rosevear on 2015/07/26.
+//  Copyright (c) 2015 Mushroom Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AAPLAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AAPLAppDelegate class]));
+    }
+}
